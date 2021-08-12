2801f7738c7a1052109299b19e268b9bf7833213
geoffreeve <49611812+geoffreeve@users.noreply.github.com>
2021-08-12 12:30:30 +1200

    update
    show all kinda finished
    checkpoint 2
    fix author link problem

--- 91902_Support_Files/L3_Site_Template/content/showall.php
+++ 91902_Support_Files/L3_Site_Template/content/showall.php
@@ -11,9 +11,59 @@ $find_rs = mysqli_fetch_assoc($find_query);
 // Loop through results and display them..
 do {
 
+    $quote = preg_replace("/[^A-Za-z0-9.,\s\'\-]/", ' ', $find_rs['Quotes']);
+
+    // author name..
+    $first = $find_rs['First'];
+    $middle = $find_rs['Middle'];
+    $last = $find_rs['Last'];
+
+    $full_name = $first." ".$middle." ".$last;
+
     ?>
 <div class="results">
-    <?php echo $find_rs['Quote']; ?>
+    <p>
+        <?php echo $quote; ?><br />
+        <a href="index.php?page=author&author_ID=<?php echo $find_rs['Author_ID'] ?>">
+            <?php echo $full_name; ?>
+        </a>
+    </p>
+    <p>
+        <?php
+            $sub1_ID = $find_rs["Subject1_ID"];
+            $sub2_ID = $find_rs["Subject2_ID"];
+            $sub3_ID = $find_rs["Subject3_ID"];
+
+            $all_subjects = array($sub1_ID, $sub2_ID, $sub3_ID);
+
+            // Loop through subject ID's and look up the subject name
+            foreach($all_subjects as $subject) {
+                // Get subject name
+                $sub_sql = "SELECT * FROM `subject` WHERE `Subject_ID` = $subject";
+                $sub_query = mysqli_query($dbconnect, $sub_sql);
+                $sub_rs = mysqli_fetch_assoc($sub_query);
+
+                if($subject != 0) {
+
+                
+
+                ?>
+            <span class="tag">
+                <a href="index.php?page=subject&subjectID=<?php echo
+                $sub_rs['Subject_ID']; ?>">
+                    <?php echo $sub_rs['Subject']; ?>
+                </a>
+            </span> &nbsp;
+        <?php
+
+                } // End subject exists if
+
+                unset($subject);
+
+            } // End subject loop
+    
+        ?>
+    </p>
 </div>
     <?php
 }   // end of display results 'do'
